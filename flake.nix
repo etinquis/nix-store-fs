69582de4e19f59203832b8bbe36fa96ec9ee6593
Martyn York <etinquis@gmail.com>
2026-09-16 10:40:14 -0700

    add flake package

--- flake.nix
+++ flake.nix
@@ -1,37 +1,69 @@
 {
   description = "A Nix-flake-based Go 1.23 development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       goVersion = 23; # Change this to update the whole stack
 
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
     in
     {
       overlays.default = final: prev: {
         go = final."go_1_${toString goVersion}";
       };
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            go
-            gcc
-            fuse
-          ];
+      packages = forEachSupportedSystem (
+        { pkgs }: rec {
+          default = nix-store-fs;
+          nix-store-fs = pkgs.buildGoModule {
+            name = "nix-store-fs";
+            src = ./src;
+            vendorHash = "sha256-UuiXA+p9LTdOfeXxkZm/ee5iDtehQ9O8Gsecfx9ZXFQ=";
+            ldflags = [
+              "-s"
+              "-w"
+            ];
+            meta = {
+              description = "FUSE filesystem exposing a filtered view of a Nix store scoped to a toplevel closure";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "nix-store-fs";
+            };
+          };
+        }
+      );
 
-          hardeningDisable = [ "fortify" ];
-        };
+      devShells = forEachSupportedSystem (
+        { pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gcc
+              fuse
+            ];
 
-      });
+            hardeningDisable = [ "fortify" ];
+          };
+
+        }
+      );
     };
 }
-
